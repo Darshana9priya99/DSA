5ca7a90e221bf0efe04ac1d5f5ec7edec85f2cdc
Darshana Priya <dp3251@srmist.edu.in>
2025-07-23 22:46:50 +0530

    Added Functions.java file

--- Functions.java
+++ Functions.java
@@ -1,13 +1,21 @@
 import java.util.*;
 public class Functions{
-    public static void printMyName(String name){
-        System.out.print(name);
-        return ;
-    }
+    //public static void printMyName(String name){
+        //System.out.print(name);
+       // return ;
+
+       public static int calculateSum(int a,int b){
+        int sum= a+b;
+        return sum;
+       }
+
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        String name= sc.next();
+        //String name= sc.next(); 
+        int a =sc.nextInt();
+        int b =sc.nextInt();
         
-        printMyName(name);
-    }
-}
+       // printMyName(name);
+       int sum = calculateSum(a,b);
+       System.out.println(sum);
+    }}
