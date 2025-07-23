eb31cbdf749dace531a0e7139e97bc94101699e2
Darshana Priya <dp3251@srmist.edu.in>
2025-07-23 23:01:18 +0530

    Added Functions.java file

--- Functions.java
+++ Functions.java
@@ -4,9 +4,13 @@ public class Functions{
         //System.out.print(name);
        // return ;
 
-       public static int calculateSum(int a,int b){
-        int sum= a+b;
-        return sum;
+       //public static int calculateSum(int a,int b){
+        //int sum= a+b;
+        //return sum;
+
+        public static int productNo(int a , int b){
+        int product = a*b;
+        return product;
        }
 
     public static void main(String args[]){
@@ -16,6 +20,9 @@ public class Functions{
         int b =sc.nextInt();
         
        // printMyName(name);
-       int sum = calculateSum(a,b);
-       System.out.println(sum);
+      // int sum = calculateSum(a,b);
+       //System.out.println(sum);
+
+       int product = productNo(a,b);
+       System.out.println(product);
     }}
